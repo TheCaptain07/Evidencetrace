c7e1b015b96e8c512e57ba296f93514acb497b2b
Harsh Shah <harsh300777@gmail.com>
2026-09-11 16:47:03 +0530

    Update evidencetrace_demo_updated.py

--- evidencetrace_demo_updated.py
+++ evidencetrace_demo_updated.py
@@ -1452,4 +1452,7 @@ EvidenceTrace research prototype · Designed for cybersecurity assurance and GRC
 
 
 if __name__ == "__main__":
-    demo.launch(share=True)
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=7860
+    )
